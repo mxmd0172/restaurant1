1a8bdfda472d72c75a747f480678d1305b6de3ea
Mxrciano <6047013@mborijnland.nl>
2025-03-24 13:16:01 +0100

    hakk

---
+++ php/index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Responsive Header</title>
+    <link rel="stylesheet" href="../css/main.css">
+    <script href="../js/java.js"></script>
+</head>
+
+    <?php 
+    include 'header.php';
+    ?>
+
+<?php 
+    include 'footer.php'; 
+    ?>
+    
